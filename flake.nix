5e06c2fb87d4e1d14990474d81a59ed2bee30a4c
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-09 11:54:07 +0200

    drop flake-utils follow: rs-harbor migrated to flake-parts

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay.follows = "rs-harbor/rust-overlay";
-    flake-utils.follows = "rs-harbor/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
@@ -21,7 +21,6 @@
     plinth = {
       url = "git+https://codeberg.org/caniko/plinth.git?ref=refs/heads/trunk";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
