cb4ac7bd2fd1a11a45e658fe68f64db991d91402
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-30 15:47:17 +0200

    feat: adopt rs-harbor toolchainProfile

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     rs-harbor = {
-      url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=9bfa8bdb0ecb22d7bc11448665f7fbaebae7a759";
+      url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=c26b735eede8078f795651c4a9cbf0be8733b221";
     };
 
     nixpkgs.follows = "rs-harbor/nixpkgs";
@@ -43,7 +43,7 @@
 
       toolchain = rs-harbor.lib.mkToolchain {
         inherit pkgs;
-        channel = "stable";
+        toolchainProfile = "nightly";
         extensions = ["rustfmt" "clippy"];
         withRustAnalyzer = false;
         crossTargets = [];
