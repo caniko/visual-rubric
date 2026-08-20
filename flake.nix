abe81aac6bc7834499de663a9a324e607047f5b1
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-20 13:32:15 +0200

    chore: bump harbor pins to new rev

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     rs-harbor = {
-      url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=77d0a937c760e6ced8b7ec8fc5a214f550abe35e";
+      url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
     };
 
     nixpkgs.follows = "rs-harbor/nixpkgs";
