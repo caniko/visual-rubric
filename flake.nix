ed10290980f65ca8075a23803c4d17c4a083be74
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-09-09 20:17:30 +0200

    chore: pin harbor-rs to fac8049

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     harbor-rs = {
-      url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
+      url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=fac8049316846e0ef1c1e6acd92aed7a337b333a";
     };
     rs-harbor.follows = "harbor-rs";
 
