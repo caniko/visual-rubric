2f2e7935826969101707d917af63ee0d536e4a21
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-09-17 09:04:43 +0200

    chore: migrate harbor inputs to github and update flake locks

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     harbor-rs = {
       url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=fac8049316846e0ef1c1e6acd92aed7a337b333a";
     };
-    rs-harbor.follows = "harbor-rs";
 
     nixpkgs.follows = "harbor-rs/nixpkgs";
     rust-overlay.follows = "harbor-rs/rust-overlay";
